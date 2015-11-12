4cf3afdcf7609ac5a646f5bd3e9206f4a15152ac
Jack Abernethy <jabber26@gmail.com>
2015-11-12 21:37:34 +0000

    Complete 4.3.2

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,15 @@
+def add(num, ber)
+   return num + ber
+end
+
+def subtract(num, ber)
+   return num - ber
+end
+
+def multiply(num, ber)
+   return num * ber
+end
+
+def divide(num, ber)
+   return num / ber.to_f
+end

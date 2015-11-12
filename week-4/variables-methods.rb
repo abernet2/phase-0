a7a4bedafc646892360c7afc5f2e44edf66799a4
Jack Abernethy <jabber26@gmail.com>
2015-11-12 01:56:12 +0000

    Add partially completed challenge 4.3

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,14 @@
+puts "Yo gimme yur 1st name"
+first = gets.chomp
+puts "Please provide me with your middle name please"
+middle = gets.chomp
+puts "Now your last name, if you will"
+last = gets.chomp
+puts "Thanks "+first+" "+middle+" "+last+" now I'm going to go steal your identity hahah!"
+
+
+puts "What is your FAVORITE number?"
+fav = gets.chomp
+fav = fav.to_i
+
+
